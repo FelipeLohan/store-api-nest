17b5e2610bb41dc97dcf3d9d46757b6e8a9b2ca4
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-12 10:32:34 -0300

    feat(controllers): created users controller

---
+++ src/users.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from "@nestjs/common";
+
+@Controller("/users")
+export class UsersController {
+
+  @Post()
+  async newUser(@Body() userData){
+    return userData;
+  };
+
+}

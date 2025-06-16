a3671cb7f1b1d986f5f5332f625540000bf97436
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-16 10:05:51 -0300

    feat(controller): added Product DTO to validation controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post, Get } from "@nestjs/common";
 import { ProductsRepository } from "./products.repository";
+import { ProductDTO } from "./dtos/ProductDTO";
 
 @Controller("/products")
 export class ProductsController{
@@ -7,7 +8,7 @@ export class ProductsController{
   constructor(private productRepo: ProductsRepository){}
 
   @Post()
-  async newProduct(@Body() product){
+  async newProduct(@Body() product: ProductDTO){
     this.productRepo.save(product);
 
     return product;

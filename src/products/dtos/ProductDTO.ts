a08cc8837709111aaa7f029b4dd53181503572e3
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-16 10:05:12 -0300

    feat(dtos): added new Product DTO

---
+++ src/products/dtos/ProductDTO.ts
@@ -0,0 +1,31 @@
+import { IsArray, IsNotEmpty, IsNumber, IsString, Min, ValidateNested } from "class-validator";
+import { CharacteristicsProductDTO } from "./CharacteristicsProductDTO";
+import { ImageProductDTO } from "./ImageProductDTO";
+import { Type } from "class-transformer";
+
+export class ProductDTO {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  value: number;
+
+  @Min(1)
+  quantity: number;
+
+  @IsString()
+  description: string;
+
+  @ValidateNested()
+  @IsArray()
+  @Type(() => CharacteristicsProductDTO)
+  characteristics: CharacteristicsProductDTO[];
+
+  @IsArray()
+  @Type(() => ImageProductDTO)
+  images: ImageProductDTO[];
+
+  @IsString()
+  categories: string;
+}

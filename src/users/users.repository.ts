cd0a233d6d19b30dd25a2ddb4477e8f1340d74a4
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-17 10:40:47 -0300

    feat(repository): added new function to check email exists in db

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import { UserDTO } from "./dtos/UserDTO";
 
 @Injectable()
 export class UsersRepository{
-  private usersData: any = [];
+  private usersData: UserDTO[] = [];
 
   async save(usersData){
     this.usersData.push(usersData);
@@ -12,5 +13,11 @@ export class UsersRepository{
 
   async listAll() {
     return this.usersData;
+  }
+
+  async emailExits(email){
+    const user = this.usersData.find(user => user.email === email);
+
+    return user !== undefined;
   }
 }

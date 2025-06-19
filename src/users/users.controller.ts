e0f6e5139c5af0e21feea6121742d8ba56aaee37
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-19 10:49:01 -0300

    feat(controller): added id for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsersRepository } from "./users.repository";
+import { UserEntity } from "./users.entity";
 import { UserDTO } from "./dtos/UserDTO";
+import { v4 as uuid } from "uuid";
 
 @Controller("/users")
 export class UsersController {
@@ -9,9 +11,11 @@ export class UsersController {
 
   @Post()
   async newUser(@Body() userData: UserDTO){
-    this.userRepo.save(userData);
+    const userEntity: UserEntity = { ...userData , id: uuid() };
 
-    return userData;
+    this.userRepo.save(userEntity);
+
+    return userEntity;
   };
 
   @Get()

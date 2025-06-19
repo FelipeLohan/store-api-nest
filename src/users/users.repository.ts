e83b770188f61594af7c968016c182636e0aa880
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-19 10:48:31 -0300

    refactor(repository): added Users types

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { UserDTO } from "./dtos/UserDTO";
+import { UserEntity } from "./users.entity";
 
 @Injectable()
 export class UsersRepository{
-  private usersData: UserDTO[] = [];
+  private usersData: UserEntity[] = [];
 
-  async save(usersData){
+  async save(usersData: UserEntity){
     this.usersData.push(usersData);
 
     console.log(this.usersData);

310ab1814dbeae79238e5ae1692c19178ff3e8ab
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-20 10:46:08 -0300

    feat(repository): added find by id

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -1,19 +1,27 @@
-import { Injectable } from "@nestjs/common";
-import { ProductEntity } from "./products.entity";
+import { Injectable } from '@nestjs/common';
+import { ProductEntity } from './products.entity';
 
 @Injectable()
-export class ProductsRepository{
-  
+export class ProductsRepository {
   private productsData: ProductEntity[] = [];
-  
-  async save(product: ProductEntity){
+
+  async save(product: ProductEntity) {
     this.productsData.push(product);
 
     return this.productsData;
   }
 
-  
-  async listAll(){
+  private findById(id: string) {
+    const product = this.productsData.find((product) => product.id === id);
+
+    if (!product) {
+      throw new Error('Produto não existe');
+    }
+
+    return product;
+  }
+
+  async listAll() {
     return this.productsData;
   }
 }

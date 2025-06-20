a8f6dc211a204eb2cdbbefa77278d2564c1bce96
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-20 09:40:32 -0300

    refactor(controller): refactored listallusers , using DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,33 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { UsersRepository } from "./users.repository";
-import { UserEntity } from "./users.entity";
-import { UserDTO } from "./dtos/UserDTO";
-import { v4 as uuid } from "uuid";
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { UserEntity } from './users.entity';
+import { UserDTO } from './dtos/UserDTO';
+import { v4 as uuid } from 'uuid';
+import { UserListDTO } from './dtos/UserListDTO';
 
-@Controller("/users")
+@Controller('/users')
 export class UsersController {
-
-  constructor(private userRepo: UsersRepository){}
+  constructor(private userRepo: UsersRepository) {}
 
   @Post()
-  async newUser(@Body() userData: UserDTO){
-    const userEntity: UserEntity = { ...userData , id: uuid() };
+  async newUser(@Body() userData: UserDTO) {
+    const userEntity: UserEntity = { ...userData, id: uuid() };
 
     this.userRepo.save(userEntity);
 
     return userEntity;
-  };
+  }
 
   @Get()
   async listAllUsers() {
-    return this.userRepo.listAll();
+    const savedUsers: UserEntity[] = await this.userRepo.listAll();
+    
+    const usersList: UserListDTO[] = savedUsers.map(
+      (user) => new UserListDTO(user.name, user.id),
+    );
+
+    return usersList;
   }
+
+ 
 }

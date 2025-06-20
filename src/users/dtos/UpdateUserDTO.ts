d2c7ae933bfc252eef3a51f3d486974657d473b4
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-20 09:59:19 -0300

    feat(dtos): added Update User dto

---
+++ src/users/dtos/UpdateUserDTO.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import { IsUniqueEmail } from '../validators/unique-user-email.validator';
+
+export class UpdateUserDTO {
+  @IsString({ message: 'O nome deve ser uma String.' })
+  @IsNotEmpty({ message: 'O nome é obrigatório. ' })
+  @IsOptional()
+  name: string;
+
+  @IsEmail(undefined, { message: 'Esse email é inválido.' })
+  @IsUniqueEmail({message: "Esse email já está cadastrado."})
+  @IsOptional()
+  email: string;
+
+  @MinLength(6, { message: 'A senha deve conter no mínimo 6 caracteres' })
+  @IsOptional()
+  password: string;
+}

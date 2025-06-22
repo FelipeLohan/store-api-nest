50f8bf977775fe1fb09f2926b17da2dbdd6eea7a
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-22 16:05:23 -0300

    feat(module): added typeOrm and service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,13 @@ import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
 import { UsersRepository } from "./users.repository";
 import { UniqueUserEmailValidator } from "./validators/unique-user-email.validator";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "./users.entity";
+import { UserService } from "./users.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [UsersRepository, UniqueUserEmailValidator]
+  providers: [UsersRepository, UniqueUserEmailValidator, UserService]
 })
 export class UsersModule {}

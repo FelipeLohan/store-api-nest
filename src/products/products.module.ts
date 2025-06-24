0abd39a2268bbb7b6bd4c6efc51c1fa8cb6f1573
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-24 10:55:35 -0300

    feat(module): added ProductEntity to module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ProductsController } from "./products.controller";
 import { ProductsRepository } from "./products.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProductEntity } from "./products.entity";
+import { ProductService } from "./product.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
   controllers: [ProductsController],
-  providers: [ProductsRepository]
+  providers: [ProductsRepository, ProductService]
 })
 export class ProductsModule{}

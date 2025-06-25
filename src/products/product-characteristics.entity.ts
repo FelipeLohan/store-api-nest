6f1548fab1f809a3b79b2d7ea7a957770aa18954
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-25 10:13:41 -0300

    feat(entity): added product relation table

--- src/products/product-characteristics.entity.ts
+++ src/products/product-characteristics.entity.ts
@@ -1,14 +1,21 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ProductEntity } from './products.entity';
 
-@Entity({ name: "product_characteristics"})
-export class ProductCharacteristics {
-  
+@Entity({ name: 'product_characteristics' })
+export class ProductCharacteristicsEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  
-  @Column({ name: "name", length: 70, nullable: false})
+
+  @Column({ name: 'name', length: 70, nullable: false })
   name: string;
 
-  @Column({ name: "description", length: 255, nullable: false})
+  @Column({ name: 'description', length: 255, nullable: false })
   description: string;
+
+  @ManyToOne(
+    () => ProductEntity,
+    (productEntity) => productEntity.characteristics,
+    { orphanedRowAction: 'delete', onDelete: 'CASCADE', onUpdate: 'CASCADE' },
+  )
+  product: ProductEntity;
 }
--- src/products/product-images.entity.ts
+++ src/products/product-images.entity.ts
@@ -1,14 +1,21 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ProductEntity } from './products.entity';
 
-@Entity({ name: "product_images"})
-export class ProductImage {
-
+@Entity({ name: 'product_images' })
+export class ProductImageEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  
-  @Column({ name: "url", nullable: false})
+
+  @Column({ name: 'url', nullable: false })
   url: string;
 
-  @Column({ name: "description", length: 255, nullable: false})
+  @Column({ name: 'description', length: 255, nullable: false })
   description: string;
+
+  @ManyToOne(() => ProductEntity, (productEntity) => productEntity.images, {
+    orphanedRowAction: 'delete',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  product: ProductEntity;
 }

3d67c236ecc0cb69c282bd14304d191148990677
FelipeLohan <felipelohan03052006@gmail.com>
2025-06-25 10:12:43 -0300

    feat(entity): added relations in characteristics and images table

--- src/products/products.entity.ts
+++ src/products/products.entity.ts
@@ -1,36 +1,37 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ProductCharacteristicsEntity } from './product-characteristics.entity';
+import { ProductImageEntity } from './product-images.entity';
 
-class CharacteristicsProduct {
-  name: string;
-  description: string;
-}
-
-class ImageProduct {
-  url: string;
-  description: string;
-}
-
-@Entity({ name: "products" })
+@Entity({ name: 'products' })
 export class ProductEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ name: "name", length: 100, nullable: false })
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
 
-  @Column({ name: "price", nullable: false })
+  @Column({ name: 'price', nullable: false })
   value: number;
 
-  @Column({ name: "quantity", nullable: false })
+  @Column({ name: 'quantity', nullable: false })
   quantity: number;
 
-  @Column({ name: "description", length: 255, nullable: false })
+  @Column({ name: 'description', length: 255, nullable: false })
   description: string;
 
-  @Column({ name: "categories", length: 255, nullable: false })
+  @Column({ name: 'categories', length: 255, nullable: false })
   categories: string;
 
-  
-  //characteristics: CharacteristicsProduct[];
-  //images: ImageProduct[];
+  @OneToMany(
+    () => ProductCharacteristicsEntity,
+    (productCharacteristics) => productCharacteristics.product,
+    { cascade: true, eager: true },
+  )
+  characteristics: ProductCharacteristicsEntity[];
+
+  @OneToMany(() => ProductImageEntity, (productImage) => productImage.product, {
+    cascade: true,
+    eager: true,
+  })
+  images: ProductImageEntity[];
 }
